9e0c2207cdc3d082a687933e11a64136024c5962
jamil quituizaca <geraldjt.8@gmail.com>
2023-05-26 16:18:46 -0500

    taller completo

--- ejemplo02/ingreso_datos.py
+++ ejemplo02/ingreso_datos.py
@@ -1,49 +1,30 @@
-from traceback import print_list
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-# se importa la clase(s) del 
-# archivo genera_tablas
 from genera_tablas import Club, Jugador
 
-# se importa información del archivo configuracion
 from configuracion import cadena_base_datos
-# se genera enlace al gestor de base de
-# datos
-# para el ejemplo se usa la base de datos
-# sqlite
+
 engine = create_engine(cadena_base_datos)
 
 Session = sessionmaker(bind=engine)
 session = Session()
 
-# se crea un objetos de tipo Club 
-club1 = Club(nombre="Barcelona", deporte="Fútbol", \
-        fundacion=1920)
+archivo = open('data/datos_clubs.txt', 'r')
 
-club2 = Club(nombre="Emelec", deporte="Fútbol", \
-        fundacion=1930)
+lista = archivo.readlines()
 
-club3 = Club(nombre="Liga de Quito", deporte="Fútbol", \
-        fundacion=1940)
+lista = [l.replace("\n", "").split(";") for l in lista]
 
-# Se crean objeto de tipo Jugador
-#
-club1 = club1(nombre="Barcelona")
-Club 
+for l in lista: 
+    nombre = l[0]
+    deporte = l[1]
+    anio = l[2]
+    print(nombre, deporte, anio)
 
+    club1 = Club(nombre=nombre, deporte=deporte, \
+        fundacion=anio)
+    
+    session.add(club1)
 
-# se agrega los objetos
-# a la sesión
-session.add(club1)
-session.add(club2)
-session.add(club3)
-session.add(jugador1)
-session.add(jugador2)
-session.add(jugador3)
-session.add(jugador4)
-session.add(jugador5)
-session.add(jugador6)
-
-# se confirma las transacciones
 session.commit()
